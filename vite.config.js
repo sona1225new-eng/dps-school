0fc28b9c724b79327479774b5d205518ad2fd141
sonali kumari <sona1225.new@gmail.com>
2026-08-08 16:40:45 +0530

    allow host

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,12 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react' // or your framework plugin
 
 export default defineConfig({
-  plugins: [react()]
-});
+  plugins: [react()],
+  preview: {
+    allowedHosts: true,
+  },
+  server: {
+    allowedHosts: true,
+  }
+})
